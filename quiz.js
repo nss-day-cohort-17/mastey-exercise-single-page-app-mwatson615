a8430b24c7d76fc3793c33eb03111e864cef5df3
Michael Watson <m.watson.tn@gmail.com>
2016-12-11 16:52:05 -0600

    added xhr and inner html return

--- quiz.js
+++ quiz.js
@@ -1,18 +1,23 @@
 
-
-
-var cars;
-function getCars() {
+function loadInventory() {
 
 var myCarsFile = new XMLHttpRequest();
-myCarsFile.addEventListener("load", loadInventory);
+myCarsFile.addEventListener("load", populatePage);
 myCarsFile.open("GET", "https://spa-cars-mastery-exercise.firebaseio.com/.json");
 myCarsFile.send();
 }
 
-getCars()
-
-function loadInventory(e) {
-	cars = JSON.parse(e.target.responseText);
-	console.log(cars)
+function populatePage(e) {
+	carObj = JSON.parse(e.target.responseText);
+	var carDisplay = "";
+	for (i = 0; i < carObj.cars.length; i++) {
+		carDisplay += `<div class="card card-block">
+									<h4>${carObj.cars[i].year} ${carObj.cars[i].make} ${carObj.cars[i].model}</h4>
+									<h4>$${carObj.cars[i].price}</h4>
+									<h5>${carObj.cars[i].description}</h5>
+						</div>`
+console.log(carDisplay)
+document.getElementById("carCards").innerHTML = carDisplay;
+	}
 }
+loadInventory();
