9915892c0a28d012a0838b653b9ca9eeed262a2a
Michael Watson <m.watson.tn@gmail.com>
2016-12-11 21:09:52 -0600

    styled cards and begun single evt function but not finished

--- quiz.js
+++ quiz.js
@@ -1,4 +1,6 @@
 
+//XHR REQUEST
+
 function loadInventory() {
 
 var myCarsFile = new XMLHttpRequest();
@@ -6,18 +8,72 @@ myCarsFile.addEventListener("load", populatePage);
 myCarsFile.open("GET", "https://spa-cars-mastery-exercise.firebaseio.com/.json");
 myCarsFile.send();
 }
+//*********************//
+
+//VARIABLES
+
+var carObj;
+var carDisplay = "";
+var textInput = document.getElementById("textBox");
+var carCounter = 0;
+//********************//
 
 function populatePage(e) {
 	carObj = JSON.parse(e.target.responseText);
-	var carDisplay = "";
 	for (i = 0; i < carObj.cars.length; i++) {
-		carDisplay += `<div class="card card-block">
-									<h4>${carObj.cars[i].year} ${carObj.cars[i].make} ${carObj.cars[i].model}</h4>
-									<h4>$${carObj.cars[i].price}</h4>
-									<h5>${carObj.cars[i].description}</h5>
-						</div>`
-console.log(carDisplay)
+		carDisplay += `<article class="card card-block">
+							<h4>${carObj.cars[i].year} ${carObj.cars[i].make} ${carObj.cars[i].model}</h4>
+							<h5>$${carObj.cars[i].price}</h5>
+							<h5>${carObj.cars[i].description}</h5>
+						</article>`
+	carCounter += 1;
 document.getElementById("carCards").innerHTML = carDisplay;
+console.log(carDisplay)
 	}
 }
 loadInventory();
+
+//add a function that resets the border thickness and background color for each 
+//car element back to the original values
+function resetBorder() {
+
+}
+
+//changes thickness of border of car element and background color, must accept
+//2 arguments...#1 is car DOM element that was clicked...#2 is color name of
+//choice
+function changeThickness() {
+
+}
+
+//function that only creates all eventhandlers for appl
+//click of car element
+//bind description to text input when start typing
+
+ 
+// document.querySelector("body").addEventListener("click", function activateEvents(e) {
+// 	//when click on car element, clear the value of text input in navbar, and put
+// 	//cursor into nav bar
+// 	if (e.target.classList.contains("card")) {
+// 		textInput = "";
+// 	} else if (){
+
+// 	}
+
+// })
+// }
+//function to reset border thickness and background color for each element back to original values
+
+
+//EVENT HANDLER//
+
+
+// if() {
+
+// }
+
+//************************//
+
+
+
+// document.getElementById("showInventory").addEventListener("click", loadInventory);
