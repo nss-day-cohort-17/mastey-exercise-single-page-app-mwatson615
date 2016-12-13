390a6fab431d8f46d67e5b78b12b601e04055de6
Michael Watson <m.watson.tn@gmail.com>
2016-12-12 22:39:26 -0600

    fixed targeting of cards evt listener

--- quiz.js
+++ quiz.js
@@ -13,7 +13,7 @@ myCarsFile.send();
 //VARIABLES
 
 var carObj;
-var carDisplay = "";
+var carDisplay = [];
 var textInput = document.getElementById("textBox");
 var carCounter = 0;
 //********************//
@@ -21,15 +21,16 @@ var carCounter = 0;
 function populatePage(e) {
 	carObj = JSON.parse(e.target.responseText);
 	for (i = 0; i < carObj.cars.length; i++) {
-		carDisplay += `<article class="card card-block">
+		carDisplay += `<article class="card card-block" id="cardId">
 							<h4>${carObj.cars[i].year} ${carObj.cars[i].make} ${carObj.cars[i].model}</h4>
 							<h5>$${carObj.cars[i].price}</h5>
 							<h5>${carObj.cars[i].description}</h5>
 						</article>`
 	carCounter += 1;
 document.getElementById("carCards").innerHTML = carDisplay;
-console.log(carDisplay)
+
 	}
+// console.log(carDisplay)
 }
 loadInventory();
 
@@ -51,17 +52,29 @@ function changeThickness() {
 //bind description to text input when start typing
 
  
-// document.querySelector("body").addEventListener("click", function activateEvents(e) {
+document.querySelector("body").addEventListener("click", function activateEvents(e) {
 // 	//when click on car element, clear the value of text input in navbar, and put
 // 	//cursor into nav bar
-// 	if (e.target.classList.contains("card")) {
-// 		textInput = "";
-// 	} else if (){
+//need to target all elements in div, not just
+	if (e.target.classList.contains("card") || e.target.parentElement.classList.contains("card")) {
+		document.getElementById("textBox").focus();
+		document.getElementById("textBox").value = "";
+		e.target.classList.add("firstStyle");
+	} else if (!e.target.parentElement.classList.contains("active") ) {
+		e.target.classList.onToggle("firstStyle");
 
-// 	}
+	// 	// e.target.parentElement.setAttribute("class", "active");
 
-// })
+	// }
+	// } else if (e.target){
+
+
+}
+console.log(e)
 // }
+
+})
+// })
 //function to reset border thickness and background color for each element back to original values
 
 
