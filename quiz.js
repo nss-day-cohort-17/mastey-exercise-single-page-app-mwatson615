8fe1a0a554130f192a6e84e6afc2d0316819a277
Michael Watson <m.watson.tn@gmail.com>
2016-12-21 14:40:26 -0600

    beginning 2nd change card function arguments

--- quiz.js
+++ quiz.js
@@ -22,7 +22,7 @@ var carCounter = 0;
 function populatePage(e) {
 	carObj = JSON.parse(e.target.responseText);
 	for (i = 0; i < carObj.cars.length; i++) {
-		carDisplay += `<article class="card card-block">
+		carDisplay += `<article class="card card-block col-md-3">
 							<h4>${carObj.cars[i].year} ${carObj.cars[i].make} ${carObj.cars[i].model}</h4>
 							<h5>$${carObj.cars[i].price}</h5>
 							<h5>${carObj.cars[i].description}</h5>
@@ -34,12 +34,6 @@ document.getElementById("carCards").innerHTML = carDisplay;
 // console.log(carDisplay)
 }
 loadInventory();
-
-//add a function that resets the border thickness and background color for each 
-//car element back to the original values
-// function resetBorder() {
-
-// }
 
 //changes thickness of border of car element and background color, must accept
 //2 arguments...#1 is car DOM element that was clicked...#2 is color name of
@@ -58,6 +52,21 @@ loadInventory();
 document.querySelector("body").addEventListener("click", function activateEvents(e) {
 carArray = document.querySelectorAll(".card");
 
+// function searchDescription() {
+// 	for (i = 0; i < carArray.length; i++)
+// 		var objDescription = carObj.cars.description;
+// 		var searchInput = document.getElementById("textBox")
+// 	if (objDescription == searchInput) {
+// 		document.getElementById("textBox").innerHTML = objDescription;
+// 		// 	function(data) {
+// 		// 		return data.code === carObj.cars[i].description
+// 		// 	})
+// 		// carObj.cars[i].description
+// 	}
+// 		console.log(objDescription)
+// }
+// searchDescription();
+
 function resetBorder() {
 	for (i = 0; i < carArray.length; i++) {
 		carArray[i].classList.remove("value", "firstStyle")
@@ -66,21 +75,23 @@ function resetBorder() {
 }
 resetBorder();
 var cardElement = document.getElementsByTagName("article");
-var cardChange = document.getElementsByClassName("firstStyle");
+var cardChange = document.getElementsByClassName("");
 function changeBorder(element, color) {
 	for (i = 0; i < carArray.length; i++) {
 	if (e.target.classList.contains("card")) {
 			document.getElementById("textBox").focus();
 			document.getElementById("textBox").value = "";
-			e.target.setAttribute("class", "firstStyle card");
+			e.target.setAttribute("class", "firstStyle card col-md-3");
 	} else if (e.target.parentElement.classList.contains("card")) {
 			document.getElementById("textBox").focus();
 			document.getElementById("textBox").value = "";
-			e.target.parentElement.setAttribute("class", "firstStyle card");
-	}
+			e.target.parentElement.setAttribute("class", "firstStyle card col-md-3");
+	} else if (i > 2)
+		e.target.setAttribute("class", "firstStyle card row col-md-3")
+		e.target.parentElement.setAttribute("class", "firstStyle card row col-md-3")
 }
-console.log(carArray)
+// console.log(carArray)
 }
-changeBorder(cardElement, cardChange);
+changeBorder(element, "blue");
 
 })
