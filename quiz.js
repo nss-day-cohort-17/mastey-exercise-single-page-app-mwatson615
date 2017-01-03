2ee9cce8b91a337094e87cf663a327e74561f2db
Michael Watson <m.watson.tn@gmail.com>
2017-01-02 18:37:13 -0600

    revision of changedescription

--- quiz.js
+++ quiz.js
@@ -14,34 +14,61 @@ myCarsFile.send();
 
 var carObj;
 var carDisplay;
-var textInput = document.getElementById("textBox");
+var carInsert;
 var carCounter = 0;
+var newDiv = document.createElement("div");
+var firstDiv = document.getElementById("carCards");
+var textInput = document.getElementById("textBox");
+var carDesciption = document.getElementsByClassName("description");
+
 
 //********************//
+
+function replaceDescription() {
+	textInput.input()
+
+}
 
 function populatePage(e) {
 	carObj = JSON.parse(e.target.responseText);
 	for (i = 0; i < carObj.cars.length; i++) {
-		carDisplay += `<article class="card card-block col-md-3">
+		carCounter++;
+
+		// function format(newDiv, firstDiv) {
+				// carDisplay += `<div>`
+			carDisplay += `<div class="card card-block col-lg-3">
 							<h4>${carObj.cars[i].year} ${carObj.cars[i].make} ${carObj.cars[i].model}</h4>
 							<h5>$${carObj.cars[i].price}</h5>
-							<h5>${carObj.cars[i].description}</h5>
-						</article>`
-	carCounter += 1;
-document.getElementById("carCards").innerHTML = carDisplay;
+							<h5 class="description">${carObj.cars[i].description}</h5>
+							</div>`
+			// if (carCounter % 4 === 1) {
+			// 	// carDisplay += `</div>`
+			// }
+		// if (carCounter % 2 === 0) {
 
+		// 	carDisplay[i].parentElement.nextSibling.
+		// }
+		// var carinsert = '<div>';
+		// carInsert += carDisplay[i] + '</div>'
+		// carInsert += carDisplay[i]
+		// carInsert += '</div>'
+		// newDiv.appendChild(carDisplay)
+		// 	firstDiv.parentNode.insertBefore(newDiv, firstDiv.nextSibling);
+		// document.body.insertBefore
+		// if (i && (i % 3 === 0)) {
+
+
+		}
+
+
+document.getElementById("carCards").innerHTML = carDisplay;
+// var carDesciption = document.getElementsByClassName("description");
+console.log(carDesciption[1].innerHTML)
 	}
 // console.log(carDisplay)
-}
 loadInventory();
 
-//changes thickness of border of car element and background color, must accept
-//2 arguments...#1 is car DOM element that was clicked...#2 is color name of
-//choice
-// function changeThickness(clickedCarDOMelem, colorName) {
-// 	for(i = 0; i < carArray.length; i++)
 
-// }
 
 //function that only creates all eventhandlers for appl
 //click of car element
@@ -51,21 +78,7 @@ loadInventory();
 
 document.querySelector("body").addEventListener("click", function activateEvents(e) {
 carArray = document.querySelectorAll(".card");
-
-// function searchDescription() {
-// 	for (i = 0; i < carArray.length; i++)
-// 		var objDescription = carObj.cars.description;
-// 		var searchInput = document.getElementById("textBox")
-// 	if (objDescription == searchInput) {
-// 		document.getElementById("textBox").innerHTML = objDescription;
-// 		// 	function(data) {
-// 		// 		return data.code === carObj.cars[i].description
-// 		// 	})
-// 		// carObj.cars[i].description
-// 	}
-// 		console.log(objDescription)
-// }
-// searchDescription();
+console.log(carArray)
 
 function resetBorder() {
 	for (i = 0; i < carArray.length; i++) {
@@ -75,24 +88,36 @@ function resetBorder() {
 
 }
 resetBorder();
-var cardElement = document.getElementsByClassName("firstStyle");
+
+//changes thickness of border of car element and background color, must accept
+//2 arguments...#1 is car DOM element that was clicked...#2 is color name of
+//choice
 function changeBorder(e, color) {
 	for (i = 0; i < carArray.length; i++) {
+		var textInput = document.getElementById("textBox");
+		var carDesciption = document.querySelectorAll(".description");
+		carCounter += 1;
 	if (e.target.classList.contains("card")) {
-			document.getElementById("textBox").focus();
-			document.getElementById("textBox").value = "";
-			e.target.setAttribute("class", "firstStyle card col-md-3 " + color);
+			textInput.focus();
+			textInput.value = "";
+			carDesciption = textInput.value;
+			e.target.setAttribute("class", "firstStyle card col-lg-4 " + color);
 	} else if (e.target.parentElement.classList.contains("card")) {
-			document.getElementById("textBox").focus();
-			document.getElementById("textBox").value = "";
-			e.target.parentElement.setAttribute("class", "firstStyle card col-md-3 " + color);
-	// } else if (i > 2)
-	// 	e.target.setAttribute("class", "firstStyle card row col-md-3")
-	// 	e.target.parentElement.setAttribute("class", "firstStyle card row col-md-3 secondStyle")
+			textInput.focus();
+			textInput.value = "";
+			e.target.parentElement.setAttribute("class", "firstStyle card col-lg-4 " + color);
+			carDesciption[i] += document.getElementById("textBox").value;
 }
-}
-console.log(carArray)
-}
-changeBorder(e, "red");
 
+
+}
+
+}
+
+changeBorder(e, "red");
+function replaceDescription(e) {
+	for (i = 0; i < carArray.length; i++) {
+	document.getElementById("carCards") = textInput.value;
+}
+}
 })
